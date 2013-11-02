c83f9c9a446c2e83f94ba198875dc1dd88711a40
lfdc <lfdcdicorte@gmail.com>
2013-11-02 18:06:39 -0400

    Changed .gitignore

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 
 public class MainClass {
 
 	public static void main(String args[])
 	{
-		System.out.println("Hello World");
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter your name");
+		
+		String name = scanner.nextLine();
+		
+		System.out.println("Hello " + name);
 	}
 	
 }
