f35276bdc721ecb092dc87a668851714216712a6
lfdc <lfdcdicorte@gmail.com>
2013-11-02 21:03:30 -0400

    i added stuff's with francesco

---
+++ src/FindAverage.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class FindAverage {
+
+	public static void main(String args[]) {
+		double[] marks = { Input.getNumber(), Input.getNumber(),
+				Input.getNumber(), Input.getNumber() };
+
+		double average = 0;
+
+		System.out.println(Arrays.toString(marks));
+
+		average = (marks[0] + marks[1] + marks[2] + marks[3]) / marks.length;
+
+		System.out.println("Your average is: " + average);
+		if (average >= 80) {
+
+			System.out.print("Honour Roll");
+
+		}
+		
+		if (average < 80 && average >= 50){
+			System.out.println("Not On Honour Roll");
+		
+		}
+		
+		if (average < 50){ 
+			System.out.println("FAIL");
+		}
+	}
+
+}
--- src/MainClass.java
+++ src/MainClass.java
@@ -3,23 +3,8 @@ public class MainClass {
 	public static void main(String args[]) {
 		int firstNumber = Input.getNumber();
 		int secondNumber = Input.getNumber();
-		char operator = Input.getOperator();
-		int answer = 0;
-
-		if (operator == '*') {
-			answer = firstNumber * secondNumber;
-		}
-		if (operator == '-') {
-			answer = firstNumber - secondNumber;
-		}
-		if (operator == '/') {
-			answer = firstNumber / secondNumber;
-		}
-		if (operator == '+') {
-			answer = firstNumber + secondNumber;
-		}
-
-		System.out.println(answer);
+		
+		
 	}
 
 }
